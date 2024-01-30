34f557b36c8ba250ec40737596d116912211e136
a12bb <2713204748@qq.com>
2024-01-30 19:12:11 +0800

    mapper层

---
+++ main/mapper/EmployeeMapper.java
@@ -0,0 +1,13 @@
+package com.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pojo.Employee;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Mark
+ * @date 2024/1/30
+ */
+@Repository
+public interface EmployeeMapper extends BaseMapper<Employee> {
+}
