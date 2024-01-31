6944b1933985bd6b31829318651ce5e2af1a4aa3
a12bb <2713204748@qq.com>
2024-01-31 17:46:40 +0800

    分页工具类

---
+++ main/config/MyBatisPlusPageConfig.java
@@ -0,0 +1,22 @@
+package com.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Mark
+ * @date 2024/1/31
+ */
+@Configuration
+public class MyBatisPlusPageConfig{
+
+    @Bean
+    public MybatisPlusInterceptor myBatisPlusPageInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
