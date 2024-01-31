8ece7e8b4d25b845ee79f2a94476106825e24dc0
a12bb <2713204748@qq.com>
2024-01-31 17:48:21 +0800

    增加一些新功能

--- main/controller/EmployeeController.java
+++ main/controller/EmployeeController.java
@@ -1,19 +1,18 @@
 package com.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.common.R;
 import com.pojo.Employee;
 import com.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 /**
  * @author Mark
@@ -62,5 +61,71 @@ public class EmployeeController {
     public R<String> logout(HttpServletRequest request){
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
+    }
+
+    /**
+     * 新增员工信息
+     * @param request
+     * @param employee
+     * @return
+     */
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee){
+        String password = DigestUtils.md5DigestAsHex("12345".getBytes());
+        employee.setPassword(password);
+//        employee.setCreateTime(LocalDateTime.now());
+//        employee.setUpdateTime(LocalDateTime.now());
+//        Long empId = (Long) request.getSession().getAttribute("employee");
+//        employee.setCreateUser(empId);
+//        employee.setUpdateUser(empId);
+        employeeService.save(employee);
+        return R.success("新增员工成功");
+    }
+
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page<Object>> page(int page, int pageSize, String name){
+        // 构建分页构造器
+        Page pageInfo = new Page(page, pageSize);
+        // 构建条件构造器
+        QueryWrapper<Employee> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like(StringUtils.hasLength(name), "name", name).orderByDesc("update_time");
+        // 执行查询
+        employeeService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 更新员工状态信息
+     * @param employee
+     * @return
+     */
+    @PutMapping
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee){
+//        Long employId = (Long) request.getSession().getAttribute("employee");
+//        employee.setUpdateUser(employId);
+//        employee.setUpdateTime(LocalDateTime.now());
+        employeeService.updateById(employee);
+        return R.success("员工信息修改成功");
+    }
+
+    /**
+     * 对员工信息进行编辑，进行员工信息回显
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable Long id){
+        Employee employee = employeeService.getById(id);
+        if (employee == null){
+            R.error("没有当前员工信息，请重新操作");
+        }
+        return R.success(employee);
     }
 }
