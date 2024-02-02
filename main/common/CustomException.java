17df69a27f41f45f2da65723cf3634c318f2df95
a12bb <2713204748@qq.com>
2024-02-02 20:44:03 +0800

    报错处理

---
+++ main/common/CustomException.java
@@ -0,0 +1,11 @@
+package com.common;
+
+/**
+ * @author Mark
+ * @date 2024/2/2
+ */
+public class CustomException extends RuntimeException{
+    public CustomException(String msg){
+        super(msg);
+    }
+}
