85ac82c1e58441fe2e48e72847324138d69604be
a12bb <2713204748@qq.com>
2024-02-02 20:47:50 +0800

    Category映射

---
+++ main/mapper/CategoryMapper.java
@@ -0,0 +1,13 @@
+package com.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pojo.Category;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Mark
+ * @date 2024/2/2
+ */
+@Repository
+public interface CategoryMapper extends BaseMapper<Category> {
+}
