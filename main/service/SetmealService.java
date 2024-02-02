854cfba909feeba33e0dd1da351db65db914c9e0
a12bb <2713204748@qq.com>
2024-02-02 20:51:48 +0800

    Seatmeal服务层

---
+++ main/service/SetmealService.java
@@ -0,0 +1,13 @@
+package com.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.pojo.Setmeal;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Mark
+ * @date 2024/2/2
+ */
+
+public interface SetmealService extends IService<Setmeal> {
+}
