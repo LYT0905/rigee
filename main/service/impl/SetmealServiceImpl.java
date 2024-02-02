e3db1717e2363241694aba4e5617c60e74f09dd1
a12bb <2713204748@qq.com>
2024-02-02 20:53:37 +0800

    Setmeal实现类

---
+++ main/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,15 @@
+package com.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.mapper.SetmealMapper;
+import com.pojo.Setmeal;
+import com.service.SetmealService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Mark
+ * @date 2024/2/2
+ */
+@Service
+public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> implements SetmealService {
+}
