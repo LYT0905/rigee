23100538d9782bafc280b9d8c8b8944b882dcbd9
a12bb <2713204748@qq.com>
2024-02-02 20:49:15 +0800

    Setmeal映射

---
+++ main/mapper/SetmealMapper.java
@@ -0,0 +1,11 @@
+package com.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pojo.Setmeal;
+
+/**
+ * @author Mark
+ * @date 2024/2/2
+ */
+public interface SetmealMapper extends BaseMapper<Setmeal> {
+}
