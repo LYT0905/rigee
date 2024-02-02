8bb5f4f98a25b1252d9f82fdf6eb0dd89359dd13
a12bb <2713204748@qq.com>
2024-02-02 20:51:22 +0800

    Dish服务层

---
+++ main/service/DishService.java
@@ -0,0 +1,13 @@
+package com.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.pojo.Dish;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Mark
+ * @date 2024/2/2
+ */
+
+public interface DishService extends IService<Dish> {
+}
