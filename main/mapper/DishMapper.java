680d03b657703145a9ae14dfb4d4e4829373e840
a12bb <2713204748@qq.com>
2024-02-02 20:48:20 +0800

    Dish映射

---
+++ main/mapper/DishMapper.java
@@ -0,0 +1,11 @@
+package com.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pojo.Dish;
+
+/**
+ * @author Mark
+ * @date 2024/2/2
+ */
+public interface DishMapper extends BaseMapper<Dish> {
+}
