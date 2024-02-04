87d0a35b03f33fbccc02295a61152f58458e296a
a12bb <2713204748@qq.com>
2024-02-04 20:42:28 +0800

    新增功能

--- main/controller/CategoryController.java
+++ main/controller/CategoryController.java
@@ -8,6 +8,8 @@ import com.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * @author Mark
  * @date 2024/2/2
@@ -30,6 +32,12 @@ public class CategoryController {
         return R.success("新增成功");
     }
 
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
     @GetMapping("/page")
     public R<Page<Category>> page(int page, int pageSize){
         Page<Category> pageInfo = new Page<>(page, pageSize);
@@ -39,15 +47,40 @@ public class CategoryController {
         return R.success(pageInfo);
     }
 
+    /**
+     * 删除分类菜品
+     * @param ids
+     * @return
+     */
     @DeleteMapping
     public R<String> delete(Long ids){
         categoryService.remove(ids);
         return R.success("删除成功");
     }
 
+    /**
+     * 修改分类
+     * @param category
+     * @return
+     */
     @PutMapping
     public R<String> update(@RequestBody Category category){
         categoryService.updateById(category);
         return R.success("修改成功");
+    }
+
+    /**
+     * 新增套餐中的菜品
+     * @param category
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<Category>> list(Category category){
+        QueryWrapper<Category> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("type", category.getType()).
+                orderByAsc("sort", String.valueOf(category.getSort())).
+                orderByDesc("create_time", String.valueOf(category.getCreateTime()));
+        List<Category> list = categoryService.list(queryWrapper);
+        return R.success(list);
     }
 }
