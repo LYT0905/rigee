ebdb63aa356969e48da7665b5bcd7f1001fdebb0
a12bb <2713204748@qq.com>
2024-02-05 11:14:13 +0800

    地址管理

---
+++ main/mapper/AddressBookMapper.java
@@ -0,0 +1,11 @@
+package com.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pojo.AddressBook;
+
+/**
+ * @author Mark
+ * @date 2024/2/5
+ */
+public interface AddressBookMapper extends BaseMapper<AddressBook> {
+}
---
+++ main/service/AddressBookService.java
@@ -0,0 +1,11 @@
+package com.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.pojo.AddressBook;
+
+/**
+ * @author Mark
+ * @date 2024/2/5
+ */
+public interface AddressBookService extends IService<AddressBook> {
+}
